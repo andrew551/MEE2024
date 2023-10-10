f3eb4ac38d42f20632444b1d22307081e146567a
andrew551 <55510212+andrew551@users.noreply.github.com>
2023-10-10 01:12:09 +0100

    Windows batch files
    For installing the required Python libraries

---
+++ windows_setup.bat
@@ -0,0 +1,8 @@
+pip install scipy
+pip install astropy
+pip install matplotlib
+pip install photutils
+pip install scikit-image
+pip install astroquery
+pip install pysimplegui
+pip install opencv-python
---
+++ windows_update.bat
@@ -0,0 +1,9 @@
+python -m pip install --upgrade pip
+pip install -U scipy
+pip install -U astropy
+pip install -U matplotlib
+pip install -U photutils
+pip install -U scikit-image
+pip install -U astroquery
+pip install -U pysimplegui
+pip install -U opencv-python
