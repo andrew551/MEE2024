2db6c48b285735f48f12ceb5ee42fd32dac94001
andrew551 <55510212+andrew551@users.noreply.github.com>
2024-01-02 22:04:20 +0000

    test

--- test_tetra3.py
+++ test_tetra3.py
@@ -4,8 +4,7 @@ tetra3/examples/data directory.
 """
 from PIL import Image
 from pathlib import Path
-EXAMPLES_DIR = Path(__file__).parent
-
+import matplotlib.pyplot as plt
 import tetra3
 
 '''
@@ -19,16 +18,21 @@ exit()
 t3 = tetra3.Tetra3(load_database='hip_database938', debug_folder=Path(__file__).parent)
 
 # Path where images are
-print(EXAMPLES_DIR)
+EXAMPLES_DIR = Path(__file__).parent
 path = EXAMPLES_DIR / 'data'
 for impath in path.glob('*'):
     print('Solving for image at: ' + str(impath))
     with Image.open(str(impath)) as img:
+        centroids = tetra3.get_centroids_from_image(img, sigma=2, filtsize=31)
+        if 1:
+            plt.imshow(img)
+            plt.scatter(centroids[:10, 1], centroids[:10, 0])
+            plt.show()
         # Here you can add e.g. `fov_estimate`/`fov_max_error` to improve speed or a
         # `distortion` range to search (default assumes undistorted image). There
         # are many optional returns, e.g. `return_matches` or `return_visual`. A core
         # aspect of the solution is centroiding (detecting the stars in the image).
         # You can use `return_images` to get a second return value to check the
         # centroiding process, the key `final_centroids` is especially useful.
-        solution = t3.solve_from_image(img, fov_estimate=2, pattern_checking_stars=10) #distortion=[-.2, .1])
+        solution = t3.solve_from_image(img, fov_estimate=5, pattern_checking_stars=10) #distortion=[-.2, .1])
     print('Solution: ' + str(solution))
