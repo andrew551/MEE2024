b48dcddb6a17a04d9f17b9e10438a35b9c78cbec
andrew551 <andrew.d.p.smith@gmail.com>
2025-04-10 04:04:12 +0100

    fix legendre (1)

--- distortion_polynomial.py
+++ distortion_polynomial.py
@@ -18,23 +18,59 @@ import zipfile
 import statsmodels.api as sm
 
 mapping = {'constant':0, 'linear':1, 'quadratic':2, 'cubic':3, 'quartic': 4, 'quintic':5, 'sextic': 6, 'septic':7}
+legendre_polies = [legendre(i) for i in range(max(mapping.values())+1)]
 
-def get_basis(y, x, w, m, options, use_special=False):
+def get_basis(y, x, img_shape, options, use_special=False):
+    w = max(img_shape)/2
     basis = []
     order = mapping[options['distortionOrder']]
-    if options['basis_type'] == 'polynomial' or not use_special:
+    if options['basis_type'] == 'polynomial':
         for i in range(1, order+1): # up to nth order binomials
             for j in range(i+1):
                 basis.append(y ** j * x ** (i-j) / w**i)
         return np.array(basis).T
     elif options['basis_type'] == 'legendre':
-        legendre_polies = [legendre(i) for i in range(order+1)]
+        x_scaled = 2 * x / img_shape[1]
+        y_scaled = 2 * y / img_shape[0]
+        print("LEGENDRE BASIS MINMAX XY", np.max(x_scaled), np.min(x_scaled), np.max(y_scaled), np.min(y_scaled))
+        
         for i in range(1, order+1): # up to nth order legendre binomials
             for j in range(i+1):
-                basis.append(legendre_polies[j](y) * legendre_polies[i-j](x) / w**i)
+                if i == 1:
+                    basis.append(y ** j * x ** (i-j) / w) # special case: the linear terms need to be like this or else code elsewhere will break
+                else:
+                    basis.append(legendre_polies[j](y_scaled) * legendre_polies[i-j](x_scaled))
         return np.array(basis).T
     else:
         raise Exception("invalid basis_type")
+
+# TODO test the legendre case of this function more
+def get_constant_and_linear_terms(coeff_x, coeff_y, img_shape, options):
+    w = max(img_shape)/2
+    '''
+    given the coefficients and basis type get the four linear terms in the expansion about (0,0)
+    '''
+    constant = [coeff_x[0], coeff_y[0]]
+    x_deri = [coeff_x[1] / w, coeff_x[2] / w]
+    y_deri = [coeff_y[1] / w, coeff_y[2] / w]
+    order = mapping[options['distortionOrder']]
+    print("BASIS XY DERI", x_deri, y_deri)
+    if options['basis_type'] == 'legendre': # other coefficients in legendre basis also contribute linearly
+        ind = 3
+        for i in range(2, order+1): # up to nth order legendre binomials
+            for j in range(i+1):
+                const_ind = legendre_polies[j].coef[-1] * legendre_polies[i-j].coef[-1]
+                deriv_y = (legendre_polies[j].coef[-1] * legendre_polies[i-j].coef[-2]) if len(legendre_polies[i-j].coef) > 1 else 0
+                deriv_x = (legendre_polies[j].coef[-2] * legendre_polies[i-j].coef[-1]) if len(legendre_polies[j].coef) > 1 else 0
+                constant[0] += const_ind * coeff_x[ind]
+                constant[1] += const_ind * coeff_y[ind]
+                x_deri[0] += deriv_x * coeff_x[ind] * 2 / img_shape[1]
+                x_deri[1] += deriv_y * coeff_x[ind] * 2 / img_shape[1]
+                y_deri[0] += deriv_x * coeff_y[ind] * 2 / img_shape[0]
+                y_deri[1] += deriv_y * coeff_y[ind] * 2 / img_shape[0]
+                ind += 1
+    print(x_deri, y_deri)
+    return constant, x_deri, y_deri
 
 def get_coeff_names(options):
     names = ['1']
@@ -60,19 +96,21 @@ def _regression_helper(errors, basis_x, basis_y):
     res_x = reg_x.predict(basis_x) - errors[:, 1]
     res_y = reg_y.predict(basis_y) - errors[:, 0]
     rms = np.mean(res_x**2+res_y**2)**0.5
-    return rms
+    return rms    
 
 '''
 absorb two constant and two linear degrees of freedom in (reg_x, reg_y) into shifts in
 shifts in q
 returns: corrected q
 '''
-def _get_corrected_q(q, reg_x, reg_y, w):
-    platescale_multiplier = ((1 + reg_x.params[1] / w) * (1 + reg_y.params[2] / w))**0.5
+def _get_corrected_q(q, coeff_x, coeff_y, img_shape, options):
+    constant, x_deri, y_deri = get_constant_and_linear_terms(coeff_x, coeff_y, img_shape, options)
+    w = (max(img_shape)/2)
+    platescale_multiplier = ((1 + x_deri[0]) * (1 + y_deri[1]))**0.5
     new_platescale = q[0] * platescale_multiplier
     theta = q[3]
-    shiftRA_DEC = q[0] * np.array([[1/np.cos(q[2]), 0], [0, 1]]) @ np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta),  np.cos(theta)]]) @ np.array([reg_x.params[0], reg_y.params[0]])
-    shift_roll_angle = reg_x.params[2] / w # small angle appromixation
+    shiftRA_DEC = q[0] * np.array([[1/np.cos(q[2]), 0], [0, 1]]) @ np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta),  np.cos(theta)]]) @ np.array(constant)
+    shift_roll_angle = x_deri[1] # small angle appromixation
     corrected_q = (new_platescale, q[1] + shiftRA_DEC[0], q[2] + shiftRA_DEC[1], q[3]-shift_roll_angle)
     return corrected_q
 
@@ -82,8 +120,6 @@ date_guess : the guessed date which we want to now improve
 return : improved date guess, pmotion correction
 '''
 def _date_guess(date_guess, q, plate, stardata, img_shape, options):
-    w = (max(img_shape)/2) # 1 # for astrometrica convention
-    m = 1 #q[0] # for astrometrica convention
     '''
     target = stardata.get_vectors()
     pmotion = stardata.get_pmotion()
@@ -122,7 +158,7 @@ def _date_guess(date_guess, q, plate, stardata, img_shape, options):
 
     dtt = np.linspace(-15, 15, num=40)
     rmss = []
-    basis = get_basis(plate[:, 0], plate[:, 1], w, m, options)
+    basis = get_basis(plate[:, 0], plate[:, 1], img_shape, options)
     t0 = date_string_to_float(date_guess)
     for dt in dtt:
         stardata_copy = copy.copy(stardata)
@@ -166,10 +202,10 @@ plate: (x, y) coordinates of stars
 target: corresponding(x', y', z') of star true positions according to catalogue
 new Oct'24: option for weighted centroids 
 '''
-def _cubic_helper(q, plate, target, w, m, fix_coeff_x, fix_coeff_y, options, use_special=False, weights=1):
+def _cubic_helper(q, plate, target, img_shape, fix_coeff_x, fix_coeff_y, options, use_special=False, weights=1):
     detransformed = transforms.detransform_vectors(q, target)
     errors = detransformed - plate
-    basis = get_basis(plate[:, 0], plate[:, 1], w, m, options, use_special)
+    basis = get_basis(plate[:, 0], plate[:, 1], img_shape, options, use_special)
 
     '''
     new: if requested, use "fixed" higher order contributions
@@ -196,40 +232,39 @@ def _cubic_helper(q, plate, target, w, m, fix_coeff_x, fix_coeff_y, options, use
         fixed_correction_x = np.einsum('ik,k->i', basis_fixed, coefficients_x)
         fixed_correction_y = np.einsum('ik,k->i', basis_fixed, coefficients_y)
   
-        errors_fixed[:, 1] -= fixed_correction_x / m
-        errors_fixed[:, 0] -= fixed_correction_y / m
-        fixed_correction[:, 1] += fixed_correction_x / m
-        fixed_correction[:, 0] += fixed_correction_y / m
+        errors_fixed[:, 1] -= fixed_correction_x
+        errors_fixed[:, 0] -= fixed_correction_y
+        fixed_correction[:, 1] += fixed_correction_x
+        fixed_correction[:, 0] += fixed_correction_y
 
     if weights == 1:
         weights = np.ones(plate.shape[0]) # TODO: unused remove!
     
-    ols_result_x = sm.OLS(errors_fixed[:, 1]*m, sm.add_constant(basis_free)).fit()
-    ols_result_y = sm.OLS(errors_fixed[:, 0]*m, sm.add_constant(basis_free)).fit()
+    ols_result_x = sm.OLS(errors_fixed[:, 1], sm.add_constant(basis_free)).fit()
+    ols_result_y = sm.OLS(errors_fixed[:, 0], sm.add_constant(basis_free)).fit()
     
     #print("OLS_X_SE", ols_result_x.HC0_se, '\n', "coeff", ols_result_x.params)
     #print("OLS_Y_SE", ols_result_y.HC0_se, '\n', "coeff", ols_result_y.params)
 
-    plate_corrected = plate + np.array([ols_result_y.predict(sm.add_constant(basis_free)), ols_result_x.predict(sm.add_constant(basis_free))]).T / m + fixed_correction
+    plate_corrected = plate + np.array([ols_result_y.predict(sm.add_constant(basis_free)), ols_result_x.predict(sm.add_constant(basis_free))]).T + fixed_correction
     
     coeff_x = list(ols_result_x.params) + list(coefficients_x)
     coeff_y = list(ols_result_y.params) + list(coefficients_y)
 
-    platescale_stdrelerror = (ols_result_x.HC0_se[1]**2 + ols_result_y.HC0_se[2]**2)**0.5 / w
+
+    platescale_stdrelerror = (ols_result_x.HC0_se[1]**2 + ols_result_y.HC0_se[2]**2)**0.5 / (max(img_shape)/2)
     #print("PTSCALE STDERR:", platescale_stderror, ' vs. ', q[0])
     
-    return _get_corrected_q(q, ols_result_x, ols_result_y, w), plate_corrected, coeff_x, coeff_y, basis, errors_fixed, ols_result_x, ols_result_y, platescale_stdrelerror
+    return _get_corrected_q(q, coeff_x, coeff_y, img_shape, options), plate_corrected, coeff_x, coeff_y, basis, errors_fixed, ols_result_x, ols_result_y, platescale_stdrelerror
 
 def apply_corrections(q, plate, coeff_x, coeff_y, img_shape, options):
-    w = (max(img_shape)/2) # 1 # for astrometrica convention
-    m = 1 #result.x[0] # for astrometrica convention
-    basis = get_basis(plate[:, 0], plate[:, 1], w, m, options)
+    basis = get_basis(plate[:, 0], plate[:, 1], img_shape, options)
     print(basis.shape)
     corr_x = np.einsum('ji,i->j', basis, coeff_x[1:]) # 1: to remove constant (which should be near-zero)
     corr_y = np.einsum('ji,i->j', basis, coeff_y[1:])
     return plate + np.c_[corr_y, corr_x]
                       
-def _do_3D_plot(plate, errors, reg_x, reg_y, img_shape, w, m, options):
+def _do_3D_plot(plate, errors, reg_x, reg_y, img_shape, options):
     fig = plt.figure()
     ax = fig.add_subplot(1, 3, 1, projection='3d')    
     ax.scatter(plate[:,1], plate[:, 0], errors[:, 1], marker='+')
@@ -242,7 +277,7 @@ def _do_3D_plot(plate, errors, reg_x, reg_y, img_shape, w, m, options):
     Y = np.linspace(-img_shape[0]/2, img_shape[0]/2, 20)
     X, Y = np.meshgrid(X, Y)
 
-    basis = get_basis(Y.flatten(), X.flatten(), w, m, options)
+    basis = get_basis(Y.flatten(), X.flatten(), img_shape, options)
     
     ### fix for fixed coeffs
     order_total = mapping[options['distortionOrder']]
@@ -283,17 +318,13 @@ def _do_3D_plot(plate, errors, reg_x, reg_y, img_shape, w, m, options):
 
 def do_cubic_fit(plate, stardata, initial_guess, img_shape, options, weights=1):
     target = stardata.get_vectors()
-    w = (max(img_shape)/2) # 1 # for astrometrica convention
-    m = 1 #result.x[0] # for astrometrica convention
-    #w = 1
-    #m = max(img_shape)
     fix_coeff_x, fix_coeff_y, fix_platescale, combined_platescale_uncertainty = _open_distortion_files(options)
     order_total = mapping[options['distortionOrder']]
     order_free = mapping[options['distortion_fixed_coefficients']] if not options['distortion_fixed_coefficients'] == 'None' else order_total
 
     if order_free == 0: # special case for only constant degree of freedom: use a linear fit, then discard the stretch/skew coefficients
-        q_corrected = _cubic_helper(initial_guess, plate, target, w, m, fix_coeff_x, fix_coeff_y, dict(options, **{'distortion_fixed_coefficients':'linear'}))[0]
-        q_corrected = _cubic_helper(q_corrected, plate, target, w, m, fix_coeff_x, fix_coeff_y, dict(options, **{'distortion_fixed_coefficients':'linear'}))[0]
+        q_corrected = _cubic_helper(initial_guess, plate, target, img_shape, fix_coeff_x, fix_coeff_y, dict(options, **{'distortion_fixed_coefficients':'linear'}))[0]
+        q_corrected = _cubic_helper(q_corrected, plate, target, img_shape, fix_coeff_x, fix_coeff_y, dict(options, **{'distortion_fixed_coefficients':'linear'}))[0]
         q_corrected = tuple([np.radians(fix_platescale/3600)]+list(q_corrected[1:4]))
         plate_corrected = apply_corrections(q_corrected, plate, list(fix_coeff_x.values()), list(fix_coeff_y.values()), img_shape, options)
         detransformed = transforms.detransform_vectors(q_corrected, target)
@@ -303,9 +334,9 @@ def do_cubic_fit(plate, stardata, initial_guess, img_shape, options, weights=1):
         #    print('mean error:', mean_error)
         return q_corrected, plate_corrected, list(fix_coeff_x.values()), list(fix_coeff_y.values()), combined_platescale_uncertainty
     
-    q_corrected = _cubic_helper(initial_guess, plate, target, w, m, fix_coeff_x, fix_coeff_y, options, weights=weights)[0]
-    q_corrected = _cubic_helper(q_corrected, plate, target, w, m, fix_coeff_x, fix_coeff_y, options, weights=weights)[0]
-    q_corrected, plate_corrected, coeff_x, coeff_y, basis, errors, reg_x, reg_y, platescale_stdrelerror = _cubic_helper(q_corrected, plate, target, w, m, fix_coeff_x, fix_coeff_y, options, weights=weights) # apply for third time to really shrink the unwanted coefficients
+    q_corrected = _cubic_helper(initial_guess, plate, target, img_shape, fix_coeff_x, fix_coeff_y, options, weights=weights)[0]
+    q_corrected = _cubic_helper(q_corrected, plate, target, img_shape, fix_coeff_x, fix_coeff_y, options, weights=weights)[0]
+    q_corrected, plate_corrected, coeff_x, coeff_y, basis, errors, reg_x, reg_y, platescale_stdrelerror = _cubic_helper(q_corrected, plate, target, img_shape, fix_coeff_x, fix_coeff_y, options, weights=weights) # apply for third time to really shrink the unwanted coefficients
 
     
     '''
@@ -321,7 +352,7 @@ def do_cubic_fit(plate, stardata, initial_guess, img_shape, options, weights=1):
         print(reg_x.params)
         print(reg_y.params)
 
-        _do_3D_plot(plate, errors, reg_x, reg_y, img_shape, w, m, options)
+        _do_3D_plot(plate, errors, reg_x, reg_y, img_shape, options)
  
     return q_corrected, plate_corrected, coeff_x, coeff_y, platescale_stdrelerror
 
@@ -414,15 +445,17 @@ def _open_distortion_files(options):
         raise Exception("input distortion files are not same order: " + str(orders))
     #show_coef_boxplot(loaded)
     coeff_x, coeff_y = dict(coeff_x), dict(coeff_y)
-    print(coeff_x)
-    print(coeff_y)
+    #print(coeff_x)
+    #print(coeff_y)
     if platescale_uncertainties:
         combined_platescale_uncertainty = np.linalg.norm(platescale_uncertainties) / len(platescale_uncertainties)
     elif len(platescales) >= 3:
         print("WARNING: no platescale uncertainty found in files, using variance of observations")
         combined_platescale_uncertainty = np.std(platescales) * (len(platescales) / (len(platescales) - 1))**0.5
     else:
-        print("WARNING: no platescale uncertainty could be made")
+        if platescales:
+            print("WARNING: no platescale uncertainty could be made")
         combined_platescale_uncertainty = -1
-    return coeff_x, coeff_y, np.mean(platescales), combined_platescale_uncertainty
+    
+    return coeff_x, coeff_y, np.mean(platescales) if platescales else -1, combined_platescale_uncertainty
     
